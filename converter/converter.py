66cae95013add14986a2c50f76ae43e4f7623625
lgbarrere <louis.gabriel.barrere@gmail.com>
2021-05-31 19:00:25 +0200

    Save an ILP in a file
    > Added un method to save the given converted ILP into a file in the saves/ folder (both created if missing)
    > Added all the required conversion lines for the ILP file format, so a converted or a saved ILP will be written in the expected form
    > Replaced "x" by "z" in the conversion to differentiate a variable from CNF-SAT as "x" and a variable from ILP as "z"

--- converter/converter.py
+++ converter/converter.py
@@ -8,6 +8,7 @@ Brief : To convert a CNF-SAT formula into its ILP version
 """
 
 
+import os
 from os import path
 import logging as lg
 from ast import literal_eval
@@ -21,9 +22,12 @@ class Converter:
     def __init__(self):
         self.file_name = "" # Name of the CNF-SAT file
         self.__converted = False # Convertion applied or not
-        self.constraints = "" # ILP constraints
+        self.__maximize = "" # CNF-SAT formula
+        self.__constraints = "" # ILP constraints
+        self.__binary = "" # Specify each CNF-SAT variable as binary
         self.nb_variables = 0 # Number of variables
         self.nb_clauses = 0 # Number of clauses
+        self.__prefix = "z"
 
 
     def _formula_to_constraints(self, lines):
@@ -32,7 +36,9 @@ class Converter:
         Return : None
         > lines : The lines from a DIMACS file format
         """
-        self.constraints = ""
+        self.__maximize = "Maximize\n"
+        self.__constraints = "Subject To\n"
+        self.__binary = "Binary\n"
         nb_clauses = 1
 
         if lines is None:
@@ -49,25 +55,38 @@ class Converter:
                         i += 1
                     self.nb_variables = literal_eval(words[i])
                     self.nb_clauses = literal_eval(words[i+1])
+
+                    # Set the CNF-SAT variables to maximize and as binaries
+                    self.__maximize += "  "
+                    self.__binary += "  "
+                    for i in range(1, self.nb_variables+1):
+                        self.__maximize += self.__prefix + str(i)
+                        self.__binary += self.__prefix + str(i)
+                        if i < self.nb_variables:
+                            self.__maximize += " + "
+                            self.__binary += " "
+                    self.__maximize += "\n"
+                    self.__binary += "\n"
                 # Start writting the constraints
                 else:
-                    self.constraints += "C" + str(nb_clauses) + " : "
+                    self.__constraints += "  C" + str(nb_clauses) + " : "
                     i = 0
                     while True:
                         # Get each variable as int (to know if it's positive)
                         val = literal_eval(words[i])
                         if val < 0:
                             # Respect the rule : "not(x) = 1 - x"
-                            self.constraints += "(1 - x" + str(abs(val)) + ")"
+                            self.__constraints += "(1 - " + self.__prefix + \
+                                                str(abs(val)) + ")"
                         elif val > 0:
-                            self.constraints += "x" + words[i]
+                            self.__constraints += self.__prefix + words[i]
                         # This condition means the line ended, break the loop
                         else:
-                            self.constraints += " >= 1\n"
+                            self.__constraints += " >= 1\n"
                             break
                         # if the line is not ended, add "+" in the constraint
                         if words[i+1] != "0":
-                            self.constraints += " + "
+                            self.__constraints += " + "
                         i += 1
                     nb_clauses += 1
         self.__converted = True
@@ -109,9 +128,35 @@ class Converter:
         Return : None
         """
         if self.is_converted():
-            print("Constraints : \n" + self.constraints)
+            print(self.__maximize + self.__constraints + self.__binary + "End")
         else:
-            lg.warning("No CNF-SAT has been converted into ILP by this converter so far.\n")
+            lg.warning("No CNF-SAT has been converted into ILP so far.\n")
+
+
+    def save_ilp_in_file(self, file_name):
+        """
+        Brief : Save a converted ILP in a file (created if missing) with a name
+        Return : None
+        > fileName : The name of the file to open
+        """
+        # If nothing has been converted earlier, don't save anything
+        if not self.is_converted():
+            lg.warning("Can't save a non-existent ILP.\n")
+            return
+        
+        save_folder_name = "saves"
+        directory = path.dirname(path.dirname(__file__))
+        path_to_folder = path.join(directory, save_folder_name)
+        path_to_file = path.join(path_to_folder, file_name)
+
+        # Create folder and/or file if missing, then save the ILP
+        os.makedirs(path_to_folder, exist_ok=True)        
+        with open(path_to_file, "w") as file:
+            lg.debug("Saving in file %s...", file_name)
+            file.write(self.__maximize)
+            file.write(self.__constraints)
+            file.write(self.__binary)
+            file.write("End")
 
 
     def convert_from_file(self, file_name):
@@ -132,6 +177,7 @@ def main():
     lg.basicConfig(level=lg.DEBUG)
     converter = Converter()
     converter.convert_from_file("test.cnf")
+    converter.save_ilp_in_file("test.ilp")
     converter.print_ilp()
     print("n = " + str(converter.nb_variables) + " m = " + str(converter.nb_clauses))
 
